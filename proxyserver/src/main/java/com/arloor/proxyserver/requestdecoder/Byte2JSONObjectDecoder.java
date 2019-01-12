92e209e7b156f241e54567949778de87ca449551
arloor <admin@arloor.com>
2019-01-12 19:35:42 +0800

    删除无用log

--- proxyserver/src/main/java/com/arloor/proxyserver/requestdecoder/Byte2JSONObjectDecoder.java
+++ proxyserver/src/main/java/com/arloor/proxyserver/requestdecoder/Byte2JSONObjectDecoder.java
@@ -7,21 +7,24 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.json.JsonObjectDecoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class Byte2JSONObjectDecoder extends SimpleChannelInboundHandler<ByteBuf> {
+    private static Logger logger= LoggerFactory.getLogger(Byte2JSONObjectDecoder.class);
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, ByteBuf byteBuf) throws Exception {
         byte[] bytes = new byte[byteBuf.readableBytes()];
         byteBuf.readBytes(bytes);
         String jsonString=new String(bytes,UTF_8);
-        System.out.println(jsonString);
+        logger.debug("请求如下： "+jsonString);
         try {
             JSONObject requestJsonObject=JSONObject.parseObject(jsonString);
             ctx.fireChannelRead(requestJsonObject);
         }catch (JSONException e){
-            System.out.println("error: "+jsonString);
+            System.out.println("error parse: "+jsonString);
             throw e;
         }
     }
