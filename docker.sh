cb89b3f045073a4dbb570b789ce37fbe5168732a
arloor <admin@arloor.com>
2019-02-14 21:14:57 +0800

    修改 docker.sh

--- docker.sh
+++ docker.sh
@@ -4,6 +4,7 @@ mvn clean
 # maven package
 mvn package
 docker build -t arloor/proxyserver:3.0 .
+mvn clean
 #上传到我的docker hub（其他用户没密码不行的
 docker push arloor/proxyserver:3.0
-#docker run  -it -p 9090:9090 arloor/proxyserver:3.0
+docker run  -it -p 9090:9090 --rm arloor/proxyserver:3.0
