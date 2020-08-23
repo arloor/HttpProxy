15354a64d94e105a5f4425a4164593948970326b
arloor <admin@arloor.com>
2020-08-23 19:04:05 +0800

    oshelper 泛型

--- src/main/java/com/arloor/forwardproxy/OsHelper.java
+++ src/main/java/com/arloor/forwardproxy/OsHelper.java
@@ -1,5 +1,6 @@
 package com.arloor.forwardproxy;
 
+
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.epoll.EpollEventLoopGroup;
 import io.netty.channel.epoll.EpollServerSocketChannel;
@@ -8,6 +9,8 @@ import io.netty.channel.kqueue.KQueueEventLoopGroup;
 import io.netty.channel.kqueue.KQueueServerSocketChannel;
 import io.netty.channel.kqueue.KQueueSocketChannel;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.ServerSocketChannel;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import org.slf4j.Logger;
@@ -19,61 +22,88 @@ import java.util.function.Function;
 
 public class OsHelper {
     private static final Logger logger = LoggerFactory.getLogger(OsHelper.class);
-    private static final OS os =parseOS();
+    private static final OS os = parseOS();
 
 
-    public static Class serverSocketChannelClazz(){
+    public static Class<? extends ServerSocketChannel> serverSocketChannelClazz() {
         return os.serverSocketChannelClazz;
     }
 
-    public static Class socketChannelClazz(){
+    public static Class<? extends SocketChannel> socketChannelClazz() {
         return os.socketChannelClazz;
     }
 
-    public static EventLoopGroup buildEventLoopGroup(int num){
-        return os.eventLoopBuilder.apply(num);
+    public static EventLoopGroup buildEventLoopGroup(int num) {
+        return os.buildEventLoopGroup(num);
     }
 
-    public static boolean isUnix(){
+    public static boolean isUnix() {
         return os.equals(OS.Unix);
     }
 
+    public static boolean isWindows() {
+        return os.equals(OS.Windows);
+    }
 
+    public static boolean isMac() {
+        return os.equals(OS.MacOS);
+    }
 
     private enum OS {
-        MacOS("mac", KQueueServerSocketChannel.class, KQueueSocketChannel.class, threadNum -> new KQueueEventLoopGroup(threadNum)),
-        Unix("unix", EpollServerSocketChannel.class, EpollSocketChannel.class, threadNum -> new EpollEventLoopGroup(threadNum)),
-        Windows("windows", NioServerSocketChannel.class, NioSocketChannel.class, threadNum -> new NioEventLoopGroup(threadNum)),
-        Other("other", NioServerSocketChannel.class, NioSocketChannel.class, threadNum -> new NioEventLoopGroup(threadNum));
+        MacOS("mac", KQueueServerSocketChannel.class, KQueueSocketChannel.class) {
+            @Override
+            EventLoopGroup buildEventLoopGroup(int num) {
+                return new KQueueEventLoopGroup(num);
+            }
+        },
+        Unix("unix", EpollServerSocketChannel.class, EpollSocketChannel.class) {
+            @Override
+            EventLoopGroup buildEventLoopGroup(int num) {
+                return new EpollEventLoopGroup(num);
+            }
+        },
+        Windows("windows", NioServerSocketChannel.class, NioSocketChannel.class) {
+            @Override
+            EventLoopGroup buildEventLoopGroup(int num) {
+                return new NioEventLoopGroup(num);
+            }
+        },
+        Other("other", NioServerSocketChannel.class, NioSocketChannel.class) {
+            @Override
+            EventLoopGroup buildEventLoopGroup(int num) {
+                return new NioEventLoopGroup(num);
+            }
+        };
 
         String name;
-        Class serverSocketChannelClazz;
-        Class socketChannelClazz;
-        Function<Integer, EventLoopGroup> eventLoopBuilder;
+        Class<? extends ServerSocketChannel> serverSocketChannelClazz;
+        Class<? extends SocketChannel> socketChannelClazz;
 
-        OS(String name, Class serverSocketChannelClass, Class socketChannelClass, Function<Integer, EventLoopGroup> integerEventLoopGroupFunction) {
+        abstract EventLoopGroup buildEventLoopGroup(int num);
+
+        OS(String name, Class<? extends ServerSocketChannel> serverSocketChannelClass, Class<? extends SocketChannel> socketChannelClass) {
             this.name = name;
             this.serverSocketChannelClazz = serverSocketChannelClass;
             this.socketChannelClazz = socketChannelClass;
-            this.eventLoopBuilder = integerEventLoopGroupFunction;
         }
     }
 
     private static OsHelper.OS parseOS() {
         String osName = System.getProperty("os.name");
-        logger.info("当前系统为： "+osName);
+        logger.info("当前系统为： " + osName);
         String name = ManagementFactory.getRuntimeMXBean().getName();
         String pid = name.split("@")[0];
         logger.info("该进程pid= " + pid);
         osName = Optional.ofNullable(osName).orElse("").toLowerCase();
-        if ((osName.indexOf("win") >= 0)) {
+        if ((osName.contains("win"))) {
             return OS.Windows;
-        } else if (osName.indexOf("mac") >= 0) {
+        } else if (osName.contains("mac")) {
             return OS.MacOS;
-        } else if (osName.indexOf("nix") >= 0 || osName.indexOf("nux") >= 0 || osName.indexOf("aix") > 0) {
+        } else if (osName.contains("nix") || osName.contains("nux") || osName.indexOf("aix") > 0) {
             return OS.Unix;
         } else {
             return OS.Other;
         }
     }
 }
+
