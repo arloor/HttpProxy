42756d5f40b4e9798a241afe53f3d8e5c1a2d465
arloor <admin@arloor.com>
2020-09-12 11:35:50 +0800

    使用thyleaf渲染html

--- src/main/java/com/arloor/forwardproxy/GlobalTrafficMonitor.java
+++ src/main/java/com/arloor/forwardproxy/GlobalTrafficMonitor.java
@@ -1,6 +1,7 @@
 package com.arloor.forwardproxy;
 
 import com.alibaba.fastjson.JSONObject;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import io.netty.channel.ChannelHandler;
 import io.netty.handler.traffic.GlobalTrafficShapingHandler;
@@ -55,7 +56,6 @@ public class GlobalTrafficMonitor extends GlobalTrafficShapingHandler {
         super(executor);
     }
 
-
     @Override
     protected void doAccounting(TrafficCounter counter) {
         long lastWriteThroughput = counter.lastWriteThroughput();
@@ -67,156 +67,157 @@ public class GlobalTrafficMonitor extends GlobalTrafficShapingHandler {
     }
 
     public static final String html() {
-        String legend ="上行网速";
+        String legend = "上行网速";
         String legends = JSONObject.toJSONString(Lists.newArrayList(legend));
         String scales = JSONObject.toJSONString(xScales);
         String series = JSONObject.toJSONString(yScales);
 
-        String template = "<!DOCTYPE html>\n" +
-                "<html lang=\"en\">\n" +
-                "<head>\n" +
-                "    <meta charset=\"UTF-8\">\n" +
-                "    <title>Netty-Server</title>\n" +
-                "    <script src=\"https://cdn.staticfile.org/echarts/4.8.0/echarts.min.js\"></script>\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "<div id=\"main\" style=\"width: 100%;height: 800px;\"></div>\n" +
-                "<script type=\"text/javascript\">\n" +
-                "    // 基于准备好的dom，初始化echarts实例\n" +
-                "    var myChart = echarts.init(document.getElementById('main'));\n" +
-                "    // 指定图表的配置项和数据\n" +
-                "var option = {\n" +
-                "    title: {\n" +
-                "        text: '网速监控'\n" +
-                "    },\n" +
-                "    tooltip: {\n" +
-                "        trigger: 'axis',\n" +
-                "        formatter: function(value) {\n" +
-                "            //这里的value[0].value就是我需要每次显示在图上的数据\n" +
-                "            if (value[0].value <= 0) {\n" +
-                "                value[0].value = '0B';\n" +
-                "            } else {\n" +
-                "                var k = 1024;\n" +
-                "                var sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];\n" +
-                "                //这里是取自然对数，也就是log（k）（value[0].value），求出以k为底的多少次方是value[0].value\n" +
-                "                var c = Math.floor(Math.log(value[0].value) / Math.log(k));\n" +
-                "                value[0].value = (value[0].value / Math.pow(k, c)).toPrecision(3) + ' ' + sizes[c];\n" +
-                "            }\n" +
-                "            //这里的value[0].name就是每次显示的name\n" +
-                "            return value[0].name + \"<br/>\" + \"流量为:\" + value[0].value;\n" +
-                "        }\n" +
-                "    },\n" +
-                "    legend: {\n" +
-                "        data: "+legends+"\n" +
-                "    },\n" +
-                "    toolbox: {\n" +
-                "        feature: {\n" +
-                "            mark: {\n" +
-                "                show: true\n" +
-                "            },\n" +
-                "            dataView: {\n" +
-                "                show: true,\n" +
-                "                readOnly: false\n" +
-                "            },\n" +
-                "            magicType: {\n" +
-                "                show: true,\n" +
-                "                type: ['line', 'bar']\n" +
-                "            },\n" +
-                "            restore: {\n" +
-                "                show: true\n" +
-                "            },\n" +
-                "            saveAsImage: {\n" +
-                "                show: true\n" +
-                "            }\n" +
-                "        }\n" +
-                "    },\n" +
-                "    xAxis: {\n" +
-                "        type: 'category',\n" +
-                "        boundaryGap: false,\n" +
-                "        data: "+scales+"\n" +
-                "    },\n" +
-                "    yAxis: {\n" +
-                "        type: \"value\",\n" +
-                "        max: function(value) {\n" +
-                "            var k = 1024;\n" +
-                "            var c = Math.floor(Math.log(value.max) / Math.log(k));\n" +
-                "            interval = Math.pow(k, c);\n" +
-                "            return Math.ceil(value.max / interval) * interval;\n" +
-                "        },\n" +
-                "        interval:1024*1024,\n" +
-                "        axisLabel: {\n" +
-                "            formatter: function(value, index) {\n" +
-                "                if (value <= 0) {\n" +
-                "                    value = '0B';\n" +
-                "                } else {\n" +
-                "                    var k = 1024;\n" +
-                "                    var sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];\n" +
-                "                    //这里是取自然对数，也就是log（k）（value），求出以k为底的多少次方是value\n" +
-                "                    var c = Math.floor(Math.log(value) / Math.log(k));\n" +
-                "                    value = (value / Math.pow(k, c)).toPrecision(3) + ' ' + sizes[c];\n" +
-                "                }\n" +
-                "                //这里的value[0].name就是每次显示的name\n" +
-                "                return value;\n" +
-                "            }\n" +
-                "        },\n" +
-                "    },\n" +
-                "    series: [{\n" +
-                "        \"data\": "+series+",\n" +
-                "        \"markLine\": {\n" +
-                "            \"data\": [{\n" +
-                "                \"type\": \"average\",\n" +
-                "                \"name\": \"平均值\"\n" +
-                "            }],\n" +
-                "            \"label\": {\n" +
-                "                formatter: function(value) {\n" +
-                "                    if (value.value <= 0) {\n" +
-                "                        value.value = '0B';\n" +
-                "                    } else {\n" +
-                "                        var k = 1024;\n" +
-                "                        var sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];\n" +
-                "                        //这里是取自然对数，也就是log（k）（value），求出以k为底的多少次方是value\n" +
-                "                        var c = Math.floor(Math.log(value.value) / Math.log(k));\n" +
-                "                        value = (value.value / Math.pow(k, c)).toPrecision(3) + ' ' + sizes[c];\n" +
-                "                    }\n" +
-                "                    //这里的value[0].name就是每次显示的name\n" +
-                "                    return value;\n" +
-                "                }\n" +
-                "            }\n" +
-                "        },\n" +
-                "        \"markPoint\": {\n" +
-                "            \"data\": [{\n" +
-                "                \"type\": \"max\",\n" +
-                "                \"name\": \"最大值\"\n" +
-                "            }],\n" +
-                "            symbol:\"roundRect\",\n" +
-                "            symbolSize:[70,30],"+
-                "            \"label\": {\n" +
-                "                formatter: function(value) {\n" +
-                "                    if (value.value <= 0) {\n" +
-                "                        value.value = '0B';\n" +
-                "                    } else {\n" +
-                "                        var k = 1024;\n" +
-                "                        var sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];\n" +
-                "                        //这里是取自然对数，也就是log（k）（value），求出以k为底的多少次方是value\n" +
-                "                        var c = Math.floor(Math.log(value.value) / Math.log(k));\n" +
-                "                        value = (value.value / Math.pow(k, c)).toPrecision(3) + ' ' + sizes[c];\n" +
-                "                    }\n" +
-                "                    //这里的value[0].name就是每次显示的name\n" +
-                "                    return value;\n" +
-                "                }\n" +
-                "            }\n" +
-                "        },\n" +
-                "        \"name\": \"上行网速\",\n" +
-                "        \"smooth\": false,\n" +
-                "        \"type\": \"line\"\n" +
-                "    }],\n" +
-                "    animationDuration: 10\n" +
-                "};" +
-                "    // 使用刚指定的配置项和数据显示图表。\n" +
-                "    myChart.setOption(option);\n" +
-                "</script>\n" +
-                "</body>\n" +
-                "</html>";
-        return template;
+        return RenderUtil.text(template, ImmutableMap.of("legends", legends, "scales", scales, "series", series));
     }
+
+        private static final String template = "<!DOCTYPE html>\n" +
+            "<html lang=\"en\">\n" +
+            "<head>\n" +
+            "    <meta charset=\"UTF-8\">\n" +
+            "    <title>Netty-Server</title>\n" +
+            "    <script src=\"https://cdn.staticfile.org/echarts/4.8.0/echarts.min.js\"></script>\n" +
+            "</head>\n" +
+            "<body>\n" +
+            "<div id=\"main\" style=\"width: 100%;height: 800px;\"></div>\n" +
+            "<script type=\"text/javascript\">\n" +
+            "    // 基于准备好的dom，初始化echarts实例\n" +
+            "    var myChart = echarts.init(document.getElementById('main'));\n" +
+            "    // 指定图表的配置项和数据\n" +
+            "var option = {\n" +
+            "    title: {\n" +
+            "        text: '网速监控'\n" +
+            "    },\n" +
+            "    tooltip: {\n" +
+            "        trigger: 'axis',\n" +
+            "        formatter: function(value) {\n" +
+            "            //这里的value[0].value就是我需要每次显示在图上的数据\n" +
+            "            if (value[0].value <= 0) {\n" +
+            "                value[0].value = '0B';\n" +
+            "            } else {\n" +
+            "                var k = 1024;\n" +
+            "                var sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];\n" +
+            "                //这里是取自然对数，也就是log（k）（value[0].value），求出以k为底的多少次方是value[0].value\n" +
+            "                var c = Math.floor(Math.log(value[0].value) / Math.log(k));\n" +
+            "                value[0].value = (value[0].value / Math.pow(k, c)).toPrecision(3) + ' ' + sizes[c];\n" +
+            "            }\n" +
+            "            //这里的value[0].name就是每次显示的name\n" +
+            "            return value[0].name + \"<br/>\" + \"流量为:\" + value[0].value;\n" +
+            "        }\n" +
+            "    },\n" +
+            "    legend: {\n" +
+            "        data: [(${legends})]\n" +
+            "    },\n" +
+            "    toolbox: {\n" +
+            "        feature: {\n" +
+            "            mark: {\n" +
+            "                show: true\n" +
+            "            },\n" +
+            "            dataView: {\n" +
+            "                show: true,\n" +
+            "                readOnly: false\n" +
+            "            },\n" +
+            "            magicType: {\n" +
+            "                show: true,\n" +
+            "                type: ['line', 'bar']\n" +
+            "            },\n" +
+            "            restore: {\n" +
+            "                show: true\n" +
+            "            },\n" +
+            "            saveAsImage: {\n" +
+            "                show: true\n" +
+            "            }\n" +
+            "        }\n" +
+            "    },\n" +
+            "    xAxis: {\n" +
+            "        type: 'category',\n" +
+            "        boundaryGap: false,\n" +
+            "        data: [(${scales})]\n" +
+            "    },\n" +
+            "    yAxis: {\n" +
+            "        type: \"value\",\n" +
+            "        max: function(value) {\n" +
+            "            var k = 1024;\n" +
+            "            var c = Math.floor(Math.log(value.max) / Math.log(k));\n" +
+            "            interval = Math.pow(k, c);\n" +
+            "            return Math.ceil(value.max / interval) * interval;\n" +
+            "        },\n" +
+            "        interval:1024*1024,\n" +
+            "        axisLabel: {\n" +
+            "            formatter: function(value, index) {\n" +
+            "                if (value <= 0) {\n" +
+            "                    value = '0B';\n" +
+            "                } else {\n" +
+            "                    var k = 1024;\n" +
+            "                    var sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];\n" +
+            "                    //这里是取自然对数，也就是log（k）（value），求出以k为底的多少次方是value\n" +
+            "                    var c = Math.floor(Math.log(value) / Math.log(k));\n" +
+            "                    value = (value / Math.pow(k, c)).toPrecision(3) + ' ' + sizes[c];\n" +
+            "                }\n" +
+            "                //这里的value[0].name就是每次显示的name\n" +
+            "                return value;\n" +
+            "            }\n" +
+            "        },\n" +
+            "    },\n" +
+            "    series: [{\n" +
+            "        \"data\": [(${series})],\n" +
+            "        \"markLine\": {\n" +
+            "            \"data\": [{\n" +
+            "                \"type\": \"average\",\n" +
+            "                \"name\": \"平均值\"\n" +
+            "            }],\n" +
+            "            \"label\": {\n" +
+            "                formatter: function(value) {\n" +
+            "                    if (value.value <= 0) {\n" +
+            "                        value.value = '0B';\n" +
+            "                    } else {\n" +
+            "                        var k = 1024;\n" +
+            "                        var sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];\n" +
+            "                        //这里是取自然对数，也就是log（k）（value），求出以k为底的多少次方是value\n" +
+            "                        var c = Math.floor(Math.log(value.value) / Math.log(k));\n" +
+            "                        value = (value.value / Math.pow(k, c)).toPrecision(3) + ' ' + sizes[c];\n" +
+            "                    }\n" +
+            "                    //这里的value[0].name就是每次显示的name\n" +
+            "                    return value;\n" +
+            "                }\n" +
+            "            }\n" +
+            "        },\n" +
+            "        \"markPoint\": {\n" +
+            "            \"data\": [{\n" +
+            "                \"type\": \"max\",\n" +
+            "                \"name\": \"最大值\"\n" +
+            "            }],\n" +
+            "            symbol:\"roundRect\",\n" +
+            "            symbolSize:[70,30]," +
+            "            \"label\": {\n" +
+            "                formatter: function(value) {\n" +
+            "                    if (value.value <= 0) {\n" +
+            "                        value.value = '0B';\n" +
+            "                    } else {\n" +
+            "                        var k = 1024;\n" +
+            "                        var sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];\n" +
+            "                        //这里是取自然对数，也就是log（k）（value），求出以k为底的多少次方是value\n" +
+            "                        var c = Math.floor(Math.log(value.value) / Math.log(k));\n" +
+            "                        value = (value.value / Math.pow(k, c)).toPrecision(3) + ' ' + sizes[c];\n" +
+            "                    }\n" +
+            "                    //这里的value[0].name就是每次显示的name\n" +
+            "                    return value;\n" +
+            "                }\n" +
+            "            }\n" +
+            "        },\n" +
+            "        \"name\": \"上行网速\",\n" +
+            "        \"smooth\": false,\n" +
+            "        \"type\": \"line\"\n" +
+            "    }],\n" +
+            "    animationDuration: 10\n" +
+            "};" +
+            "    // 使用刚指定的配置项和数据显示图表。\n" +
+            "    myChart.setOption(option);\n" +
+            "</script>\n" +
+            "</body>\n" +
+            "</html>";
 }
---
+++ src/main/java/com/arloor/forwardproxy/RenderUtil.java
@@ -0,0 +1,99 @@
+package com.arloor.forwardproxy;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.StringTemplateResolver;
+
+import java.util.Map;
+
+public class RenderUtil {
+    private final static TemplateEngine textEngine = new TemplateEngine();
+    private final static TemplateEngine htmlEngine = new TemplateEngine();
+
+    static {
+        StringTemplateResolver textResolver = new StringTemplateResolver();
+        textResolver.setOrder(1);
+        textResolver.setTemplateMode(TemplateMode.TEXT);
+        // TODO Cacheable or Not ?
+        textResolver.setCacheable(true);
+        textEngine.setTemplateResolver(textResolver);
+
+        StringTemplateResolver templateResolver = new StringTemplateResolver();
+        templateResolver.setOrder(1);
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        // TODO Cacheable or Not ?
+        templateResolver.setCacheable(true);
+        htmlEngine.setTemplateResolver(templateResolver);
+    }
+
+    /**
+     * 使用 Thymeleaf 渲染 Text模版
+     * Text模版语法见：https://www.thymeleaf.org/doc/tutorials/3.0/usingthymeleaf.html#textual-syntax
+     *
+     * @param template 模版
+     * @param params   参数
+     * @return 渲染后的Text
+     */
+    public static String text(String template, Map<String, Object> params) {
+
+        Context context = new Context();
+        context.setVariables(params);
+        return textEngine.process(template, context);
+    }
+
+    /**
+     * 使用 Thymeleaf 渲染 Html模版
+     *
+     * @param template Html模版
+     * @param params   参数
+     * @return 渲染后的html
+     */
+    public static String html(String template, Map<String, Object> params) {
+        Context context = new Context();
+        context.setVariables(params);
+        return htmlEngine.process(template, context);
+    }
+
+    /**
+     * 测试用，展示如何使用
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        // 渲染String
+        String string_template = "这是[(${name.toString()})]"; // 直接name其实就行了，这里就是展示能调用java对象的方法
+        String value = RenderUtil.text(string_template, ImmutableMap.of("name", "ARLOOR"));
+        System.out.println(value);
+
+        // 渲染List
+        /**
+         * [# th:each="item : ${items}"]
+         *   - [(${item})]
+         * [/]
+         */
+        String list_template = "[# th:each=\"item : ${items}\"]\n" +
+                "  - [(${item})]\n" +
+                "[/]";
+        String value1 = RenderUtil.text(list_template, ImmutableMap.of("items", Lists.newArrayList("第一个", "第二个")));
+        System.out.println(value1);
+
+        // 渲染Map
+        /**
+         * [# th:each="key : ${map.keySet()}"]
+         *   - [(${map.get(key)})]
+         * [/]
+         */
+        String map_template = "[# th:each=\"key : ${map.keySet()}\"]\n" +
+                " 这是 - [(${map.get(key)})]\n" +
+                "[/]";
+        String value2 = RenderUtil.text(map_template, ImmutableMap.of("map", ImmutableMap.of("a", "甲", "b", "乙")));
+        System.out.println(value2);
+
+        String html_template = "这是<span th:text=\"${name}\"></span>";
+        System.out.println(RenderUtil.html(html_template, ImmutableMap.of("name", "ARLOOR")));
+
+    }
+}
