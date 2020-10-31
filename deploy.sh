42c9dd818324588c9057a9993f8c570e8a73f9c3
arloor <admin@arloor.com>
2020-10-31 12:10:25 +0800

    默认配置

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-for host in someme.me
-do
- scp target/httpproxy-1.0-jar-with-dependencies.jar root@${host}:/opt/proxy/forwardproxy-1.0-jar-with-dependencies.jar
- ssh root@${host} "service proxy restart"
-done
