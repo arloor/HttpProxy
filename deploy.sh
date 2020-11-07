12df54cede576f8f0802247dd8ff1ce64eb1e002
arloor <admin@arloor.com>
2020-11-08 00:07:35 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+echo "输入hostname(以空格分割)"
+read hosts
+for host in $hosts
+do
+  echo "部署到${host}"
+  scp target/httpproxy-1.0-jar-with-dependencies.jar root@${host}:/opt/proxy/forwardproxy-1.0-jar-with-dependencies.jar
+  ssh root@${host} '
+  service proxy stop
+  service proxy start
+  '
+done
