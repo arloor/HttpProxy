e121992a1d5703c3cb57ed11b0cf97118fe57a3c
liuganghuan <liuganghuan@meituan.com>
2021-01-31 19:06:13 +0800

    mvn打包

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ read hosts
 for host in $hosts
 do
   echo "部署到${host}"
-  scp target/httpproxy-1.0-jar-with-dependencies.jar root@${host}:/opt/proxy/forwardproxy-1.0-jar-with-dependencies.jar
+  scp target/httpproxy-1.0-release.jar root@${host}:/opt/proxy/forwardproxy-1.0-jar-with-dependencies.jar
   ssh root@${host} '
   service proxy stop
   service proxy start
