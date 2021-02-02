e028afc7734574bff76708ef57e25461eabb5f18
liuganghuan <liuganghuan@meituan.com>
2021-02-02 12:02:31 +0800

    发布到中央仓库的配置

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 echo "输入hostname(以空格分割)"
 read hosts
-for host in $hosts
-do
+for host in $hosts; do
   echo "部署到${host}"
-  scp target/httpproxy-1.0-release.jar root@${host}:/opt/proxy/forwardproxy-1.0-jar-with-dependencies.jar
+  scp target/httpproxy-1.0-SNAPSHOT-release.jar root@${host}:/opt/proxy/forwardproxy-1.0-jar-with-dependencies.jar
   ssh root@${host} '
   service proxy stop
   service proxy start
