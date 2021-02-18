897009ff238625dba1f765b13382cf409ed3fda2
arloor <admin@arloor.com>
2021-02-18 21:23:10 +0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,10 @@ echo "输入hostname(以空格分割)"
 read hosts
 for host in $hosts; do
   echo "部署到${host}"
-  scp httpproxy-1.0-SNAPSHOT-all.jar root@${host}:/opt/proxy/forwardproxy-1.0-jar-with-dependencies.jar
+  scp target/httpproxy-1.0-SNAPSHOT-all.jar root@${host}:/opt/proxy/forwardproxy-1.0-jar-with-dependencies.jar
   ssh root@${host} '
   service proxy stop
   service proxy start
+  service proxy status --no-page
   '
 done
